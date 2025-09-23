d57da9b7c05198a3d52c65a77b5e8985efa03563
Victor Santos <menezes.victorsantos@outlook.com>
2025-09-23 05:38:26 -0300

    Feature: redesign and send reset password email component

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,18 +1,27 @@
-import { GalleryVerticalEnd } from "lucide-react";
+import { AnimatedThemeToggler } from "@/components/ui/animated-theme-toggler";
 import LoginForm from "./login-form";
+import { WavyBackground } from "@/components/ui/wavy-background";
+import { BlurFade } from "@/components/ui/blur-fade";
 
 export default function RegisterPage() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            <GalleryVerticalEnd className="size-4" />
-          </div>
-          iLium Inc.
-        </a>
-        <LoginForm />
+    <WavyBackground className="w-full">
+      <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+        <BlurFade delay={0.25} inView className="flex items-center justify-center w-full">
+        <div className="flex w-full flex-col gap-6">
+          <a
+            href="#"
+            className="flex items-center gap-2 self-center font-medium"
+          >
+            <div className="flex items-center gap-2 self-center font-medium">
+              <AnimatedThemeToggler />
+            </div>
+            Resum.it
+          </a>
+          <LoginForm />
+        </div>
+        </BlurFade>
       </div>
-    </div>
+    </WavyBackground>
   );
 }
--- app/auth/register/register-form.tsx
+++ app/auth/register/register-form.tsx
@@ -13,11 +13,22 @@ import { Input } from "@/components/ui/input";
 import z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { BorderTrail } from "@/components/motion-primitives/border-trail";
-import { LucideArrowRight, LucideMail, LucideUser } from "lucide-react";
+import {
+  LucideAlertCircle,
+  LucideArrowRight,
+  LucideCircleX,
+  LucideMail,
+  LucideUser,
+  LucideUserRoundCheck,
+} from "lucide-react";
 import { PasswordInput } from "@/components/ui/password-input";
 import { Field } from "@/components/Field";
 import { authClient, Session } from "@/lib/auth-client";
 import { RegisterDto } from "./register-dto";
+import { Spinner } from "@/components/ui/shadcn-io/spinner";
+import { useMutation } from "@tanstack/react-query";
+import { LoginDto } from "../login/login-dto";
+import { toast } from "sonner";
 const registerSchema = z
   .object({
     name: z.string().min(3),
@@ -44,29 +55,62 @@ export default function RegisterForm({
   const form = useForm<RegisterTypeSchema>({
     resolver: zodResolver(registerSchema),
   });
-  
 
-  async function handleRegisterForm(formData: RegisterDto) {
-    console.log(formData)
-    const auth = await authClient.signUp.email({
-      name: formData.name,
-      email: formData.email,
-      password: formData.password,
-      callbackURL: 'http://localhost:3000'
-    })
-    console.log(auth)
+  const registerMutation = useMutation({
+    mutationFn: async (formData: RegisterDto) => {
+      const c = await authClient.signUp.email({
+        name: formData.name,
+        email: formData.email,
+        password: formData.password,
+        callbackURL: "http://localhost:3000/auth/login",
+      });
+      if (c.error) {
+        throw c.error;
+      }
+      return c;
+    },
+    onError: (err) => {
+      toast.error("Error", {
+        position: "bottom-center",
+        style: {
+          "--normal-bg":
+            "color-mix(in oklab, var(--destructive) 30%, var(--background) 70% )",
+          "--normal-text": "var(--destructive)",
+          "--normal-border": "var(--destructive)",
+        } as React.CSSProperties,
+        icon: <LucideCircleX />,
+        description: err.message,
+      });
+    },
+    onSuccess: (data) => {
+      toast.success("Success", {
+        position: "bottom-center",
+        style: {
+          "--normal-bg":
+            "color-mix(in oklab, var(--color-green-600) 30%, var(--background) 70%)",
+          "--normal-text":
+            "light-dark(var(--color-green-600), var(--color-green-400))",
+          "--normal-border":
+            "light-dark(var(--color-green-600), var(--color-green-400))",
+        } as React.CSSProperties,
+        icon: <LucideUserRoundCheck />,
+        description: "Please check your inbox to verify your email.",
+      });
+    },
+  });
+
+  function handleRegisterForm(formData: RegisterDto) {
+    registerMutation.mutate(formData);
   } //function to handle with data and submit
   return (
-    <div className={cn("flex flex-col gap-6 relative", className)} {...props}>
-      <Card className="relative">
-        <BorderTrail
-          className="bg-linear-to-l from-[#6769ff] to-[#696bfd]"
-          style={{
-            boxShadow:
-              "0px 0px 60px 30px #6366f1, 0 0 100px 60px #6366f1, 0 0 140px 90px #6366f1",
-          }}
-          size={100}
-        />
+    <div
+      className={cn(
+        "flex flex-col gap-6 relative w-full items-center justify-center"
+      )}
+      {...props}
+    >
+      <Card className="relative min-w-1/4 max-w-1/2">
+        <BorderTrail size={100} />
         <CardHeader className="text-center">
           <CardTitle className="text-xl">Nice to meet you!</CardTitle>
 
@@ -81,7 +125,7 @@ export default function RegisterForm({
             noValidate={true}
           >
             <div className="grid gap-4">
-              <div className="grid gap-3">
+              <div className="grid gap-2">
                 <Field.Root>
                   <Field.Label requiredIcon>Name</Field.Label>
                   <Input
@@ -130,7 +174,15 @@ export default function RegisterForm({
                 </Field.Root>
                 <div className="relative">
                   <Button type="submit" className="w-full relative">
-                    Register <LucideArrowRight />{" "}
+                    {registerMutation.isPending ? (
+                      <>
+                        <Spinner variant="ring" /> Loading...
+                      </>
+                    ) : (
+                      <>
+                        Continue <LucideArrowRight />
+                      </>
+                    )}
                   </Button>
                 </div>
               </div>
