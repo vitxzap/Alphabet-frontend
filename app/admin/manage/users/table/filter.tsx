f32bfcd203aba24a785088fe36348aa021e94d15
Victor Santos <menezes.victorsantos@outlook.com>
2025-12-06 06:17:48 -0300

    feat: filters

--- app/admin/manage/users/table/filter.tsx
+++ app/admin/manage/users/table/filter.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -11,7 +12,18 @@ import {
   DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { InputGroup, InputGroupInput } from "@/components/ui/input-group";
+import { Input } from "@/components/ui/input";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import {
   Select,
   SelectContent,
@@ -21,7 +33,10 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
+  ArrowDown,
+  ChevronDown,
   CircleDashed,
   CircleUserRound,
   Notebook,
@@ -30,94 +45,115 @@ import {
   UserRoundX,
   X,
 } from "lucide-react";
+import { FormEvent, ReactNode, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import z from "zod";
+import { motion } from "motion/react";
+interface FilterProps extends React.ComponentProps<"form"> {
+  filterName: string;
+  title: string;
+  description: string;
+  icon: ReactNode;
+}
 
+const filterSchema = z.object({
+  filter: z
+    .string()
+    .nonempty()
+    .max(32)
+    .regex(/^[a-zA-Z\s]+$/, "Only letters are allowed"),
+});
+type FilterType = z.infer<typeof filterSchema>;
 export function Filter() {
+  const [open, setOpen] = useState<boolean>(false);
+  const [filterValue, setFilterValue] = useState<FilterType["filter"]>("");
+  const form = useForm<FilterType>({
+    resolver: zodResolver(filterSchema),
+    defaultValues: {
+      filter: "",
+    },
+  });
+  const submitFilter = () => {
+    console.log(form.getValues("filter"));
+    setFilterValue(form.getValues("filter"));
+  };
   return (
-    <div className="flex w-max">
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant={"ghost"} className="text-muted-fg rounded-full">
+    <div className="flex w-max gap-2">
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant={filterValue ? "default" : "outline"}
+            className={`rounded-full text-sm ${
+              filterValue ? "" : "text-muted-fg"
+            }`}
+            size={"sm"}
+          >
             {" "}
-            <Plus /> Add filter
+            <CircleUserRound />{" "}
+            {form.getValues("filter") == "" ? (
+              <>Name</>
+            ) : (
+              <>{filterValue}</>
+            )}{" "}
+            <ChevronDown />{" "}
           </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-64">
-          <DropdownMenuLabel>Filters</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuGroup>
-            <DropdownMenuSub>
-              <DropdownMenuSubTrigger>
-                <CircleUserRound /> Name
-              </DropdownMenuSubTrigger>
-              <DropdownMenuPortal>
-                <DropdownMenuSubContent className="min-w-72">
-                  <DropdownMenuLabel>Filtering name</DropdownMenuLabel>
-                  <div className="flex flex-col gap-2 p-2 w-full">
-                    <InputGroup>
-                      <InputGroupInput placeholder="Enter name..." />
-                    </InputGroup>
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-2">
+              <h4 className="leading-none font-medium">Filtering name</h4>
+              <span className="text-muted-fg text-sm">
+                This field will filter all students by name
+              </span>
+            </div>
 
-                    <div className="flex gap-1 justify-end">
-                      <Button>Apply filter</Button>
-                    </div>
-                  </div>
-                </DropdownMenuSubContent>
-              </DropdownMenuPortal>
-            </DropdownMenuSub>
+            <form
+              className="flex flex-col gap-2"
+              autoComplete="off"
+              onSubmit={form.handleSubmit(submitFilter)}
+            >
+              <Controller
+                name="filter"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <InputGroup>
+                    <InputGroupAddon>
+                      <CircleUserRound />
+                    </InputGroupAddon>
+                    <InputGroupInput
+                      placeholder="Enter filter..."
+                      {...field}
+                      autoCapitalize="true"
+                    />
+                    Input
+                  </InputGroup>
+                )}
+              />
 
-            <DropdownMenuSub>
-              <DropdownMenuSubTrigger>
-                <Notebook /> Course
-              </DropdownMenuSubTrigger>
-              <DropdownMenuPortal>
-                <DropdownMenuSubContent className="min-w-72">
-                  <DropdownMenuLabel>Filtering course</DropdownMenuLabel>
-                  <div className="flex flex-col gap-2 p-2 w-full">
-                    <InputGroup>
-                      <InputGroupInput placeholder="Enter course..." />
-                    </InputGroup>
-                    <div className="flex gap-1 justify-end">
-                      <Button>Apply filter</Button>
-                    </div>
-                  </div>
-                </DropdownMenuSubContent>
-              </DropdownMenuPortal>
-            </DropdownMenuSub>
-            <DropdownMenuSub>
-              <DropdownMenuSubTrigger>
-                <CircleDashed /> Status
-              </DropdownMenuSubTrigger>
-              <DropdownMenuPortal>
-                <DropdownMenuSubContent className="min-w-72">
-                  <DropdownMenuLabel>Filtering status</DropdownMenuLabel>
-                  <div className="flex flex-col gap-2 p-2 w-full ">
-                    <Select>
-                      <SelectTrigger className="w-full">
-                        <SelectValue placeholder={"Status"} />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectGroup>
-                          <SelectLabel>Teste</SelectLabel>
-                          <SelectItem value="verified">
-                            <UserRoundCheck /> Verified
-                          </SelectItem>
-                          <SelectItem value="unverified">
-                            <UserRoundX /> Unverified
-                          </SelectItem>
-                        </SelectGroup>
-                      </SelectContent>
-                    </Select>
-
-                    <div className="flex gap-1 justify-end">
-                      <Button>Apply filter</Button>
-                    </div>
-                  </div>
-                </DropdownMenuSubContent>
-              </DropdownMenuPortal>
-            </DropdownMenuSub>
-          </DropdownMenuGroup>
-        </DropdownMenuContent>
-      </DropdownMenu>
+              <div className="flex gap-1">
+                <Button
+                  variant={"outline"}
+                  onClick={() => {
+                    form.resetField("filter");
+                    setOpen(false);
+                    setFilterValue(form.getValues("filter"));
+                  }}
+                >
+                  Remove
+                </Button>
+                <Button
+                  className="flex-1"
+                  type="submit"
+                  onClick={() => setOpen(false)}
+                  disabled={!form.formState.isValid}
+                >
+                  Apply filter
+                </Button>
+              </div>
+            </form>
+          </div>
+        </PopoverContent>
+      </Popover>
     </div>
   );
 }
