88152267adea6a55882143b307d9da584730d42d
Victor Santos <menezes.victorsantos@outlook.com>
2025-12-06 01:43:10 -0300

    refactor(filters): design changes

--- app/admin/manage/users/table/filter.tsx
+++ app/admin/manage/users/table/filter.tsx
@@ -3,12 +3,33 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuGroup,
-  DropdownMenuItem,
   DropdownMenuLabel,
+  DropdownMenuPortal,
   DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { CircleDashed, CircleUserRound, Notebook, Plus } from "lucide-react";
+import { InputGroup, InputGroupInput } from "@/components/ui/input-group";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  CircleDashed,
+  CircleUserRound,
+  Notebook,
+  Plus,
+  UserRoundCheck,
+  UserRoundX,
+  X,
+} from "lucide-react";
 
 export function Filter() {
   return (
@@ -24,15 +45,76 @@ export function Filter() {
           <DropdownMenuLabel>Filters</DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuGroup>
-            <DropdownMenuItem>
-              <CircleUserRound /> Name
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <Notebook /> Course
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <CircleDashed /> Status
-            </DropdownMenuItem>
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger>
+                <CircleUserRound /> Name
+              </DropdownMenuSubTrigger>
+              <DropdownMenuPortal>
+                <DropdownMenuSubContent className="min-w-72">
+                  <DropdownMenuLabel>Filtering name</DropdownMenuLabel>
+                  <div className="flex flex-col gap-2 p-2 w-full">
+                    <InputGroup>
+                      <InputGroupInput placeholder="Enter name..." />
+                    </InputGroup>
+
+                    <div className="flex gap-1 justify-end">
+                      <Button>Apply filter</Button>
+                    </div>
+                  </div>
+                </DropdownMenuSubContent>
+              </DropdownMenuPortal>
+            </DropdownMenuSub>
+
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger>
+                <Notebook /> Course
+              </DropdownMenuSubTrigger>
+              <DropdownMenuPortal>
+                <DropdownMenuSubContent className="min-w-72">
+                  <DropdownMenuLabel>Filtering course</DropdownMenuLabel>
+                  <div className="flex flex-col gap-2 p-2 w-full">
+                    <InputGroup>
+                      <InputGroupInput placeholder="Enter course..." />
+                    </InputGroup>
+                    <div className="flex gap-1 justify-end">
+                      <Button>Apply filter</Button>
+                    </div>
+                  </div>
+                </DropdownMenuSubContent>
+              </DropdownMenuPortal>
+            </DropdownMenuSub>
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger>
+                <CircleDashed /> Status
+              </DropdownMenuSubTrigger>
+              <DropdownMenuPortal>
+                <DropdownMenuSubContent className="min-w-72">
+                  <DropdownMenuLabel>Filtering status</DropdownMenuLabel>
+                  <div className="flex flex-col gap-2 p-2 w-full ">
+                    <Select>
+                      <SelectTrigger className="w-full">
+                        <SelectValue placeholder={"Status"} />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectLabel>Teste</SelectLabel>
+                          <SelectItem value="verified">
+                            <UserRoundCheck /> Verified
+                          </SelectItem>
+                          <SelectItem value="unverified">
+                            <UserRoundX /> Unverified
+                          </SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+
+                    <div className="flex gap-1 justify-end">
+                      <Button>Apply filter</Button>
+                    </div>
+                  </div>
+                </DropdownMenuSubContent>
+              </DropdownMenuPortal>
+            </DropdownMenuSub>
           </DropdownMenuGroup>
         </DropdownMenuContent>
       </DropdownMenu>
